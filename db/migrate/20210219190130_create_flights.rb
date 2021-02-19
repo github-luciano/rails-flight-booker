65937bb5d41bb46c904a1f7858498223b3b03ffc
nluciano233 <nluciano233@gmail.com>
2021-02-19 20:07:19 +0100

    migration

--- db/migrate/20210219190130_create_flights.rb
+++ db/migrate/20210219190130_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
-      t.references :from_airport, null: false, foreign_key: true
-      t.references :to_airport, null: false, foreign_key: true
+      t.references :from_airport, null: false, foreign_key: { to_table: :airports} #to_table tells rails to which table the foreign key is refferring to. You generate it with "key-name:references"
+      t.references :to_airport, null: false, foreign_key: { to_table: :airports}
       t.datetime :datetime
       t.time :flight_duration
 
---
+++ db/schema.rb
@@ -0,0 +1,34 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_19_190130) do
+
+  create_table "airports", force: :cascade do |t|
+    t.text "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "from_airport_id", null: false
+    t.integer "to_airport_id", null: false
+    t.datetime "datetime"
+    t.time "flight_duration"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
+    t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
+  end
+
+  add_foreign_key "flights", "airports", column: "from_airport_id" #to make sure the foreign key was generated correclty, the add_foreign_key should look like you see there
+  add_foreign_key "flights", "airports", column: "to_airport_id"
+end
