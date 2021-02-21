af254a7c50f3d9aad45b5b6b480023956c32e830
nluciano233 <nluciano233@gmail.com>
2021-02-21 20:23:57 +0100

    changed keys

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
-  belongs_to :departing, class_name: 'Airport', foreign_key: 'from_airport_id'
-  belongs_to :arriving, class_name: 'Airport', foreign_key: 'to_airport_id'
+  belongs_to :departing_airport, class_name: 'Airport', foreign_key: 'from_airport_id'
+  belongs_to :arriving_airport, class_name: 'Airport', foreign_key: 'to_airport_id'
   has_many :flights, class_name: 'Flight', foreign_key: 'flight_id'
 
+  scope :future, -> {where(datetime > Time.now)}
 end
