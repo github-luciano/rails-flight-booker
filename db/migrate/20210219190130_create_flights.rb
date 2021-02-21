9bd3fc709fe1c6d27a0de30dea6a6ff732d4a234
nluciano233 <nluciano233@gmail.com>
2021-02-21 15:11:16 +0100

    time to int

--- db/migrate/20210219190130_create_flights.rb
+++ db/migrate/20210219190130_create_flights.rb
@@ -4,7 +4,7 @@ class CreateFlights < ActiveRecord::Migration[6.1]
       t.references :from_airport, null: false, foreign_key: { to_table: :airports} #to_table tells rails to which table the foreign key is refferring to. You generate it with "key-name:references"
       t.references :to_airport, null: false, foreign_key: { to_table: :airports}
       t.datetime :datetime
-      t.time :flight_duration
+      t.integer :flight_duration
 
       t.timestamps
     end
