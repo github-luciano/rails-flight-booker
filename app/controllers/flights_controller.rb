2f246ea5453036ec596251316f49441d30da3197
nluciano233 <nluciano233@gmail.com>
2021-02-25 21:28:04 +0100

    if date is empty all flights will be shown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,21 @@
 class FlightsController < ApplicationController
 
   def index
-    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], datetime: params[:datetime])
+    if params[:datetime] != ""
+      @available_flights = Flight.where(flight_params)
+    else
+      params.delete(:datetime)
+      @available_flights = Flight.where(flight_params)
+    #@available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], datetime: params[:datetime])
     #@flight_search = Flight.search(params[:flight])
     #@flight_options = Flight.all.map { |a| [ a.departing_airport.code, a.departing_airport.id ] }.uniq
   end
+end
 
   private
   def flight_params
-    params.require(:flight).permit(:from_airport_id, :to_airport_id, :datetime)
+    params.permit(:from_airport_id, :to_airport_id, :datetime)
+    
   end
 
 end
