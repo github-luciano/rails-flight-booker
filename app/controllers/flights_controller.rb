baac3607a79bab04bb9a9a3d565e707bc6a8b7ea
nluciano233 <nluciano233@gmail.com>
2021-02-25 19:47:09 +0100

    date not working

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,14 @@
 class FlightsController < ApplicationController
 
   def index
-    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id])
+    @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], datetime: params[:datetime])
     #@flight_search = Flight.search(params[:flight])
     #@flight_options = Flight.all.map { |a| [ a.departing_airport.code, a.departing_airport.id ] }.uniq
   end
 
   private
   def flight_params
-    params.require(:flight).permit(:from_airport_id, :to_airport_id)
+    params.require(:flight).permit(:from_airport_id, :to_airport_id, :datetime)
   end
 
 end
