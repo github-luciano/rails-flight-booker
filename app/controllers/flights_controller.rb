954955768fdeddce29f43039d0a4012d815d90f3
nluciano233 <nluciano233@gmail.com>
2021-02-26 18:19:28 +0100

    form now keeps values between searches

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,8 +2,12 @@ class FlightsController < ApplicationController
 
   def index
     if params.has_key?(:from_airport_id) && params.has_key?(:to_airport_id) #this is necessary so if the params does not have any key (so the user has not given anything to search for), then the available flights will not be rendering in the index 
-      if params[:from_airport_id] == ""
-          flash.now[:alert] = "Departure airport is missing"
+      if params[:from_airport_id] == "" && params[:to_airport_id] == ""
+        flash.now[:alert] = "Please insert departure and destination airport"
+      elsif params[:from_airport_id] == ""
+        flash.now[:alert] = "Please insert departure airport"
+      elsif params[:to_airport_id] == ""
+        flash.now[:alert] = "Please insert destination airport"
       else
         if params[:datetime] != "" #datetime gets deleted from params if it is empty, so if the user doesnt input the date he can see all the flights to the selected airportts that will take place in the future
           @available_flights = Flight.where(flight_params)
