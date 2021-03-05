7b8b662fb799b9bf12b9781e206377538aad79c6
nluciano233 <nluciano233@gmail.com>
2021-03-05 18:40:43 +0100

    idk

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,22 @@ class BookingsController < ApplicationController
 
   def new
     @flight = Flight.find_by(id: params[:flight]) #the flight params with the ID gets submitted from the flights index form. The ID that is submitted is the ID of the flight that the user has chosen with the radio button
+    @seats = params[:seats].to_i
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
     if @booking.save
-      redirect_to @booking
+      redirect_to flights_path
     else
       render :new
     end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:primary_id, :passenger2_id, :passenger3_id, :passenger4_id, :flight_id, :email, :billing, passengers_attributes: [ :name, :surname ])
   end
 
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,6 @@ class Passenger < ApplicationRecord
   has_many :passenger2, class_name: 'Booking', foreign_key: 'passenger2_id'
   has_many :passenger3, class_name: 'Booking', foreign_key: 'passenger3_id'
   has_many :passenger4, class_name: 'Booking', foreign_key: 'passenger4_id'
+  belongs_to :booking
+
 end
