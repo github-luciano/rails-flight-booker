2913c4a43dd2a6355b0197783553e6f2ee5493b3
nluciano233 <nluciano233@gmail.com>
2021-03-05 10:41:26 +0100

    accept nested attributes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
   belongs_to :passenger3, class_name: 'Passenger', foreign_key: 'passenger3_id', optional: true
   belongs_to :passenger4, class_name: 'Passenger', foreign_key: 'passenger4_id', optional: true
   belongs_to :flight, class_name: 'Flight', foreign_key: 'flight_id'
+  accepts_nested_attributes_for :passengers #you need nested attributes for passengers names. All the other info (billing,email,ecc) are stored in the bookings table, so in the table of the model and view you're using for the forms
+
 end
