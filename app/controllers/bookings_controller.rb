51b975c583f422a2eb1f811328f73e338511bbda
nluciano233 <nluciano233@gmail.com>
2021-03-05 11:03:53 +0100

    made create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,16 @@ class BookingsController < ApplicationController
 
   def new
     @flight = Flight.find_by(id: params[:flight]) #the flight params with the ID gets submitted from the flights index form. The ID that is submitted is the ID of the flight that the user has chosen with the radio button
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
   end
 
 end
