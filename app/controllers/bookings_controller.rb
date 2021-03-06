2f1a289e15a82d8a7ba1405db2a6489a44190d48
nluciano233 <nluciano233@gmail.com>
2021-03-06 15:31:20 +0100

    comment

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,14 +7,22 @@ class BookingsController < ApplicationController
   end
 
   def create
+    #I need to re-declare the variables here because if the first attempt to submit the form fails
+    #the render :new will re-render the page. That page will the new.html.erb, but the NEW ACTION 
+    #from the controller will never be called again. As a result the variables declared in the 
+    #NEW action don't have any effect since they will never get called. To pass the params that were
+    #sent on the first attempt to complete the form and that are then lost when the RENDER :NEW gets
+    #executed, I have made some hidden field tags in the new.html.erb that will re-submit those 
+    #parameters. The para
     @flight = Flight.find_by(id: params[:flight])
     @seats = params[:seats].to_i
     @booking = Booking.new(booking_params)
     if @booking.save
       redirect_to flights_path
     else
-      redirect_to new_booking_path(:flight => params[:flight], :seats => params[:seats].to_i )
-      flash.now[:alert] = "Please insert destination airport"
+      #redirect_to new_booking_path(:flight => params[:flight], :seats => params[:seats].to_i )
+      render :new
+      #flash[:alert] = "Please insert destination airport"
     end
   end
 
